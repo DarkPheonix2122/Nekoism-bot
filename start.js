57230d86de0614cb27a797637d2e377c3d02b37d
DarkPheonix2122 <66898756+DarkPheonix2122@users.noreply.github.com>
2025-07-08 15:18:46 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,7 +1,10 @@
 const { spawn } = require('child_process');
 function startBot() {
-  const bot = spawn('node', ['init.js'], { stdio: 'inherit' });
-
+  try{
+    const bot = spawn('node', ['init.js'], { stdio: 'inherit' });
+  }catch(err){
+    require("./functions/errorListener").send(err);
+  }
   bot.on('close', (code) => {
     console.log(`website exited with code ${code}. Restarting in 3 seconds...`);
     setTimeout(startBot, 3000);
