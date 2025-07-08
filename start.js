db52aaa7841bffd2e427fb3d0c070446cfa885ad
DarkPheonix2122 <66898756+DarkPheonix2122@users.noreply.github.com>
2025-07-08 13:32:13 -0400

    Create start.js

---
+++ start.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process');
+function startBot() {
+  const bot = spawn('node', ['neko.js'], { stdio: 'inherit' });
+
+  bot.on('close', (code) => {
+    console.log(`website exited with code ${code}. Restarting in 3 seconds...`);
+    setTimeout(startBot, 3000);
+  });
+}
+
+startBot();
