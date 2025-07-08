3885272e2505fbc10264ce1f291d3a5a034e97b3
DarkPheonix2122 <66898756+DarkPheonix2122@users.noreply.github.com>
2025-07-08 15:49:01 -0400

    Update start.js

--- start.js
+++ start.js
@@ -3,13 +3,13 @@ function startBot() {
   try{
     console.log("starting API");
     const bot = spawn('node', ['init.js'], { stdio: 'inherit' });
+    bot.on('close', (code) => {
+      console.log(`website exited with code ${code}. Restarting in 3 seconds...`);
+      setTimeout(startBot, 3000);
+    });
   }catch(err){
     require("./functions/errorListener").send(err);
   }
-  bot.on('close', (code) => {
-    console.log(`website exited with code ${code}. Restarting in 3 seconds...`);
-    setTimeout(startBot, 3000);
-  });
 }
 
 startBot();
